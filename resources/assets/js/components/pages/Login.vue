f3aea2b5890c57c77c07c12739c2211b7aa22007
renspoesse <renspoesse@gmail.com>
2017-01-25 09:49:52 +0100

    login

--- resources/assets/js/components/pages/Login.vue
+++ resources/assets/js/components/pages/Login.vue
@@ -2,7 +2,7 @@
 
     <div>
 
-        <alert type="warning" message="You need to be logged in to view this page." show="true" v-bind:closeable="false"></alert>
+        <alert type="warning" message="You need to be authenticated to view this page. Currently only LTI authentication is supported." show="true" v-bind:closeable="false"></alert>
 
     </div>
 
