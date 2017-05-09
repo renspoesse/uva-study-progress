efb5874da181b8fe5d3201c7e740d171b07b64ba
renspoesse <renspoesse@gmail.com>
2017-05-09 11:33:30 +0200

    feb kleuren

--- resources/assets/js/components/pages/Overview.vue
+++ resources/assets/js/components/pages/Overview.vue
@@ -24,6 +24,7 @@
                     <div class="w-lg m-x-auto" style="height: 30rem;">
                         <canvas ref="chartCredits"></canvas>
                     </div>
+                    <p class="m-t">* Please note that our prognosis has a margin of x credits</p>
                 </div>
             </div>
 
@@ -313,19 +314,39 @@
 
                 students.getCreditsExpected().then((result) => {
 
-//                    const creditsExpected0 = _.find(result.items, (value) => {return value.bsa_credits === this.student.bsa_credits && value.second_year_b1_subjects === 0});
-//                    const creditsExpected1 = _.find(result.items, (value) => {return value.bsa_credits === this.student.bsa_credits && value.second_year_b1_subjects === 1});
-//                    const creditsExpected2 = _.find(result.items, (value) => {return value.bsa_credits === this.student.bsa_credits && value.second_year_b1_subjects > 1});
+                    //                    const creditsExpected0 = _.find(result.items, (value) => {return value.bsa_credits === this.student.bsa_credits && value.second_year_b1_subjects === 0});
+                    //                    const creditsExpected1 = _.find(result.items, (value) => {return value.bsa_credits === this.student.bsa_credits && value.second_year_b1_subjects === 1});
+                    //                    const creditsExpected2 = _.find(result.items, (value) => {return value.bsa_credits === this.student.bsa_credits && value.second_year_b1_subjects > 1});
+                    //
+                    //                    const primaryColor = '#1CA8DD';
+                    //
+                    //                    let dipCategory = this.student.dip_category.split('~');
+                    //
+                    //                    dipCategory = {
+                    //
+                    //                        bsaCredits: parseInt(dipCategory[0]),
+                    //                        block1Courses: parseInt(dipCategory[1])
+                    //                    };
+
+                    // Behaalde credits: color1
+                    // Prognose: color2
+                    // Goal: color3
+
+//                    const color1 = 'rgba(255, 99, 132, 0.2)'; // Red
+//                    const color2 = 'rgba(255, 206, 86, 0.2)'; // Yellow
+//                    const color3 = 'rgba(75, 192, 192, 0.2)'; // Green
 //
-//                    const primaryColor = '#1CA8DD';
-//
-//                    let dipCategory = this.student.dip_category.split('~');
-//
-//                    dipCategory = {
-//
-//                        bsaCredits: parseInt(dipCategory[0]),
-//                        block1Courses: parseInt(dipCategory[1])
-//                    };
+//                    const color1Border = 'rgba(255, 99, 132, 1)'; // Red
+//                    const color2Border = 'rgba(255, 206, 86, 1)'; // Yellow
+//                    const color3Border = 'rgba(75, 192, 192, 1)'; // Green
+
+                    const color1 = 'rgba(135, 192, 181, .7)';
+                    const color2 = 'rgba(229, 217, 79, .4)';
+                    const color3 = 'rgba(135, 192, 181, .4)';
+
+                    const color1Border = color1;
+                    const color2Border = color2;
+                    const color3Border = color3;
 
                     const chart1 = new Chart(this.$refs.chartCredits, {
 
@@ -335,22 +356,22 @@
                             labels: [
 
                                 'Credits this year (' + this.student.second_year_credits + ')',
-                                'Prognosis (' + this.student.second_year_credits_expected + ')',
+                                'Prognosis* (' + this.student.second_year_credits_expected + ')',
                                 'Goal (' + this.student.second_year_credits_goal + ')',
                             ],
                             datasets: [{
 
                                 backgroundColor: [
 
-                                    'rgba(255, 99, 132, 0.2)', // Red
-                                    'rgba(255, 206, 86, 0.2)', // Yellow
-                                    'rgba(75, 192, 192, 0.2)', // Green
+                                    color1,
+                                    color2,
+                                    color3
                                 ],
                                 borderColor: [
 
-                                    'rgba(255,99,132,1)', // Red
-                                    'rgba(255, 206, 86, 1)', // Yellow
-                                    'rgba(75, 192, 192, 1)', // Green'
+                                    color1Border,
+                                    color2Border,
+                                    color3Border
                                 ],
                                 borderWidth: 1,
                                 data: [
@@ -425,7 +446,7 @@
                                     type: 'line',
                                     borderColor: [
 
-                                        'rgba(255, 206, 86, 1)' // Yellow
+                                        color2Border
                                     ],
                                     fill: false,
                                     label: 'Prognosis',
@@ -444,7 +465,7 @@
                                     type: 'line',
                                     borderColor: [
 
-                                        'rgba(75, 192, 192, 1)' // Green
+                                        color3Border
                                     ],
                                     fill: false,
                                     label: 'Goal',
@@ -463,14 +484,14 @@
                                     type: 'line',
                                     borderColor: [
 
-                                        'rgba(255,99,132,1)' // Red
+                                        color1Border
                                     ],
                                     fill: false,
                                     label: 'Credits this year',
                                     lineTension: 0,
                                     pointBackgroundColor: [
 
-                                        'rgba(255, 99, 132, 0.2)' // Red
+                                        color1
                                     ],
                                     spanGaps: true,
                                     data: [
@@ -517,107 +538,107 @@
                         }
                     });
 
-//                    const chart3 = new Chart(this.$refs.chartCreditsOld, {
-//
-//                        type: 'line',
-//                        data: {
-//
-//                            labels: [0, 1, 2],
-//                            datasets: [{
-//
-//                                fill: false,
-//                                lineTension: 0,
-//                                //backgroundColor: "rgba(75,192,192,0.4)",
-//                                //borderColor: "rgba(75,192,192,1)",
-//                                //borderCapStyle: 'butt',
-//                                //borderDash: [],
-//                                //borderDashOffset: 0.0,
-//                                //borderJoinStyle: 'miter',
-//                                pointBorderColor: [
-//
-//                                    dipCategory.block1Courses === 0 ? primaryColor : 'red',
-//                                    dipCategory.block1Courses === 1 ? primaryColor : 'yellow',
-//                                    dipCategory.block1Courses > 1 ? primaryColor : 'green'
-//                                ],
-//                                pointBackgroundColor: ['red', 'yellow', 'green'],
-//                                //pointBorderWidth: 1,
-//                                pointHoverRadius: [
-//
-//                                    dipCategory.block1Courses === 0 ? 20 : 5,
-//                                    dipCategory.block1Courses === 1 ? 20 : 5,
-//                                    dipCategory.block1Courses > 1 ? 20 : 5
-//                                ],
-//                                //pointHoverBackgroundColor: "rgba(75,192,192,1)",
-//                                //pointHoverBorderColor: "rgba(220,220,220,1)",
-//                                //pointHoverBorderWidth: 1,
-//                                pointRadius: [
-//
-//                                    dipCategory.block1Courses === 0 ? 10 : 2,
-//                                    dipCategory.block1Courses === 1 ? 10 : 2,
-//                                    dipCategory.block1Courses > 1 ? 10 : 2
-//                                ],
-//                                data: [
-//
-//                                    dipCategory.block1Courses === 0 ? this.student.second_year_credits_expected : creditsExpected0.second_year_credits_expected || 0,
-//                                    dipCategory.block1Courses === 1 ? this.student.second_year_credits_expected : creditsExpected1.second_year_credits_expected || 0,
-//                                    dipCategory.block1Courses > 1 ? this.student.second_year_credits_expected : creditsExpected2.second_year_credits_expected || 0
-//                                ],
-//                                //spanGaps: false,
-//                            }]
-//                        },
-//                        options: {
-//
-//                            legend: {
-//
-//                                display: false
-//                            },
-//                            maintainAspectRatio: false,
-//                            scales: {
-//
-//                                xAxes: [{
-//
-//                                    scaleLabel: {
-//
-//                                        display: true,
-//                                        labelString: 'Courses passed in 1st block'
-//                                    }
-//                                }],
-//                                yAxes: [{
-//
-//                                    ticks: {
-//
-//                                        max: 70,
-//                                        min: 0,
-//                                        stepSize: 10
-//                                    }
-//                                }]
-//                            },
-//                            title: {
-//
-//                                display: true,
-//                                text: 'Credit prognosis 2nd year - ' + this.student.bsa_credits + ' credit BSA group'
-//                            },
-//                            tooltips: {
-//
-//                                callbacks: {
-//
-//                                    title: function(tooltipItem, data) {
-//
-//                                        const passed = parseInt(tooltipItem[0].xLabel);
-//
-//                                        if (passed === 0)
-//                                            return passed + ' courses passed in block 1';
-//                                        else if (passed === 1)
-//                                            return passed + ' course passed in block 1';
-//                                        else if (passed > 1)
-//                                            return 'At least ' + passed + ' courses passed in block 1';
-//                                    },
-//                                    label: function(tooltipItem, data) { return 'Your prognosis for the 2nd year is ' + tooltipItem.yLabel + ' ECTS.'; }
-//                                },
-//                                displayColors: false
-//                            }
-//                        }
-//                    });
+                    //                    const chart3 = new Chart(this.$refs.chartCreditsOld, {
+                    //
+                    //                        type: 'line',
+                    //                        data: {
+                    //
+                    //                            labels: [0, 1, 2],
+                    //                            datasets: [{
+                    //
+                    //                                fill: false,
+                    //                                lineTension: 0,
+                    //                                //backgroundColor: "rgba(75,192,192,0.4)",
+                    //                                //borderColor: "rgba(75,192,192,1)",
+                    //                                //borderCapStyle: 'butt',
+                    //                                //borderDash: [],
+                    //                                //borderDashOffset: 0.0,
+                    //                                //borderJoinStyle: 'miter',
+                    //                                pointBorderColor: [
+                    //
+                    //                                    dipCategory.block1Courses === 0 ? primaryColor : 'red',
+                    //                                    dipCategory.block1Courses === 1 ? primaryColor : 'yellow',
+                    //                                    dipCategory.block1Courses > 1 ? primaryColor : 'green'
+                    //                                ],
+                    //                                pointBackgroundColor: ['red', 'yellow', 'green'],
+                    //                                //pointBorderWidth: 1,
+                    //                                pointHoverRadius: [
+                    //
+                    //                                    dipCategory.block1Courses === 0 ? 20 : 5,
+                    //                                    dipCategory.block1Courses === 1 ? 20 : 5,
+                    //                                    dipCategory.block1Courses > 1 ? 20 : 5
+                    //                                ],
+                    //                                //pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                    //                                //pointHoverBorderColor: "rgba(220,220,220,1)",
+                    //                                //pointHoverBorderWidth: 1,
+                    //                                pointRadius: [
+                    //
+                    //                                    dipCategory.block1Courses === 0 ? 10 : 2,
+                    //                                    dipCategory.block1Courses === 1 ? 10 : 2,
+                    //                                    dipCategory.block1Courses > 1 ? 10 : 2
+                    //                                ],
+                    //                                data: [
+                    //
+                    //                                    dipCategory.block1Courses === 0 ? this.student.second_year_credits_expected : creditsExpected0.second_year_credits_expected || 0,
+                    //                                    dipCategory.block1Courses === 1 ? this.student.second_year_credits_expected : creditsExpected1.second_year_credits_expected || 0,
+                    //                                    dipCategory.block1Courses > 1 ? this.student.second_year_credits_expected : creditsExpected2.second_year_credits_expected || 0
+                    //                                ],
+                    //                                //spanGaps: false,
+                    //                            }]
+                    //                        },
+                    //                        options: {
+                    //
+                    //                            legend: {
+                    //
+                    //                                display: false
+                    //                            },
+                    //                            maintainAspectRatio: false,
+                    //                            scales: {
+                    //
+                    //                                xAxes: [{
+                    //
+                    //                                    scaleLabel: {
+                    //
+                    //                                        display: true,
+                    //                                        labelString: 'Courses passed in 1st block'
+                    //                                    }
+                    //                                }],
+                    //                                yAxes: [{
+                    //
+                    //                                    ticks: {
+                    //
+                    //                                        max: 70,
+                    //                                        min: 0,
+                    //                                        stepSize: 10
+                    //                                    }
+                    //                                }]
+                    //                            },
+                    //                            title: {
+                    //
+                    //                                display: true,
+                    //                                text: 'Credit prognosis 2nd year - ' + this.student.bsa_credits + ' credit BSA group'
+                    //                            },
+                    //                            tooltips: {
+                    //
+                    //                                callbacks: {
+                    //
+                    //                                    title: function(tooltipItem, data) {
+                    //
+                    //                                        const passed = parseInt(tooltipItem[0].xLabel);
+                    //
+                    //                                        if (passed === 0)
+                    //                                            return passed + ' courses passed in block 1';
+                    //                                        else if (passed === 1)
+                    //                                            return passed + ' course passed in block 1';
+                    //                                        else if (passed > 1)
+                    //                                            return 'At least ' + passed + ' courses passed in block 1';
+                    //                                    },
+                    //                                    label: function(tooltipItem, data) { return 'Your prognosis for the 2nd year is ' + tooltipItem.yLabel + ' ECTS.'; }
+                    //                                },
+                    //                                displayColors: false
+                    //                            }
+                    //                        }
+                    //                    });
                 });
             }
         },
