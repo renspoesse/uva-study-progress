fcf5048c3c04c3f135420b46be4c5a1a2b9276cb
Rens Poesse <renspoesse@gmail.com>
2018-08-17 15:58:23 +0200

    Correct type

--- resources/assets/js/components/pages/Login.vue
+++ resources/assets/js/components/pages/Login.vue
@@ -2,7 +2,7 @@
 
     <div>
 
-        <alert type="info" message="You need to be authenticated to view this page. Please go (back to) Blackboard and open the StudyProgress dashboard from there!" show="true" v-bind:closeable="false"></alert>
+        <alert type="info" message="You need to be authenticated to view this page. Please go (back) to Canvas/Blackboard and open the StudyProgress dashboard from there!" show="true" v-bind:closeable="false"></alert>
         <!--<alert type="danger" message="You need to be authenticated to view this page. Currently only LTI authentication is supported." show="true" v-bind:closeable="false"></alert>-->
 
         <!--a class="btn btn-primary-outline m-b-s" href="/lti/launch/teststudent">Test as student</a><br>
