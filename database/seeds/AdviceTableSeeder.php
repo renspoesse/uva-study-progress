a9621e98dc1c9a66ff1913bebf1d1ffa03f1135d
Rens Poesse <renspoesse@gmail.com>
2018-08-28 10:09:22 -0500

    Fix advice seeder

--- database/seeds/AdviceTableSeeder.php
+++ database/seeds/AdviceTableSeeder.php
@@ -20,15 +20,18 @@ class AdviceTableSeeder extends Seeder
             'is_published' => true,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
+        ]);
 
-        ], [
+        DB::table('advice')->insert([
 
             'title' => 'How to set your goal in My Dashbooard',
             'text' => '<p>My Dashboard &gt; click on Personalise to set your goal</p><p>First reflect on how many courses you plan to pass this year. Take some time to think this through. Do you have good study habits? Do you have other commitments outside of your studies? Do you manage your time well? You are more likely to meet your goal if it is realistic for your situation. </p><p>- If you set <b>your goal above what your prognosis</b> shows you know you are in for a challenge. You may have changed your approach to your studies or are planning to do so.</p><p>- If you set <b>your goal below what your prognosis</b> shows you have chosen a slower pace. You may have personal circumstances that require you to take fewer courses. Or you may have other commitments such as work or membership in a student association.</p>',
             'is_published' => true,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now()
-        ], [
+        ]);
+
+        DB::table('advice')->insert([
 
             'title' => 'My Dashboard preferred browsers',
             'text' => '<p>‘My Dashboard’ is best used with one of the following browsers:</p><p>- Chrome</p><p>- Edge</p><p>- Safari</p><p>- Internet Explorer</p>',
