dd5fa7d294481c07a77b7d3778e3281756f2601d
Rens Poesse <renspoesse@gmail.com>
2019-06-03 15:34:00 -0500

    Quick fix to support LTI library behind proxy

--- app/Http/Controllers/LtiController.php
+++ app/Http/Controllers/LtiController.php
@@ -11,6 +11,15 @@ class LtiController extends Controller
 {
     public function index(Request $request)
     {
+        // The LTI library relies on some server variables that are not set when running behind a proxy.
+
+        $_SERVER['SERVER_NAME'] = 'studyprogress.uva.nl';
+
+        if ($request->header('X-Forwarded-Proto') === 'https') {
+            $_SERVER['HTTPS'] = 'on';
+            $_SERVER['SERVER_PORT'] = 443;
+        }
+
         $connector = $this->getConnector();
 
         // $name = '';
